2769f4c0d3b6973fbd52591cb36985340a22e7b1
Sanch9s <aleks.jurik@gmail.com>
2021-04-04 14:18:54 +0200

    remove multiple loading of products

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -50,7 +50,6 @@
 </template>
 
 <script lang="ts">
-import { RootActions } from "@/store/actions";
 import { CartActions } from "@/store/cart/actions";
 import Vue from "vue";
 import ImagesComponent from "../components/images/ImagesComponent.vue";
@@ -65,9 +64,6 @@ export default Vue.extend({
         return this.$store.getters.getProduct(Number(this.$route.params.id));
       },
     },
-  },
-  mounted() {
-    this.$store.dispatch(RootActions.LOAD_PRODUCTS);
   },
   methods: {
     goBack() {
