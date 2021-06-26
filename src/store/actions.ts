281c30f44009053fa1ef3d1f326f3323d80c1098
Sanch9s <aleks.jurik@gmail.com>
2021-06-26 17:49:53 +0200

    fix sorting

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -31,8 +31,13 @@ export const actions: ActionTree<RootState, RootState> = {
             }
 
             formattedData = formattedData.sort((a) => {
-                return a.giveaway ? -1 : a.isNew ? 1 : 0;
+                return a.isNew ? -1 : 1;
             });
+
+            formattedData = formattedData.sort((a) => {
+                return a.giveaway ? -1 : 1;
+            });
+
             context.commit(RootMutations.SET_PRODUCTS, formattedData);
         });
     }
